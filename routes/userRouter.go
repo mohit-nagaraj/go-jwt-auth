d335fe36009efedebf03b45c48af306d624cdf73
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-07-30 07:12:00 +0530

    add in routes

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,7 +1,12 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mohit-nagaraj/go-jwt-auth/middleware"
+)
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users")
+	incomingRoutes.GET("/users/:user_id")
 }
