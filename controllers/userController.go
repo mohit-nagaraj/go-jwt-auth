16119c3621ff781a142cc4a2b2908f4aee49cff3
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-08-01 11:32:05 +0530

    add get user

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,75 @@
 package controllers
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/mohit-nagaraj/go-jwt-auth/database"
+	"github.com/mohit-nagaraj/go-jwt-auth/helpers"
+	"github.com/mohit-nagaraj/go-jwt-auth/models"
+	"golang.org/x/crypto/bcrypt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
+}
+
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("email of password is incorrect")
+		check = false
+	}
+	return check, msg
+}
+
+func Signup() {
+
+}
+
+func Login() {
+
+}
+
+func GetUsers() {
+
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
+}
--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,8 +1,11 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mohit-nagaraj/go-jwt-auth/controllers"
+)
 
 func AuthRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("users/signup")
-	incomingRoutes.POST("users/login")
+	incomingRoutes.POST("users/signup", controllers.Signup())
+	incomingRoutes.POST("users/login", controllers.Login())
 }
